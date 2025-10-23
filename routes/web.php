35853d87fdb4ac2e590dce36d58b3852e10f3247
rusbear28 <rusbear28@yandex.ru>
2025-10-23 20:32:28 +0700

    ui

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes([
+    'register' => false,
+    'verify' => false,
+    'reset' => false,
+    'login' => false,
+]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
